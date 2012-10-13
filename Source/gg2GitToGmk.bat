c0e4fa24e5515082545d055144db97e44cddcf25
nagn <mlin4589@gmail.com>
2012-10-13 19:31:24 -0400

    Uhm.... I don't know, but please don't steal my personal information :(

--- Source/gg2GitToGmk.bat
+++ Source/gg2GitToGmk.bat
@@ -1,3 +1,3 @@
-del "C:\Users\Linda Lin\Desktop\GUI\chat\gg2.gmk"
-"C:\Users\Linda Lin\Desktop\GUI\gmksplit.exe" gg2 "C:\Users\Linda Lin\Desktop\GUI\chat\gg2.gmk"
+del gmk
+gmksplit.exe gg2 gg2.gmk
 pause
